59f790b5da8b1f1bb76d89f8e2c3cb99051ed4ca
jamesnottidge <freshice57@gmail.com>
2024-03-01 23:46:52 +0100

    create and update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
     end
+
   def show
     @article = Article.find(params[:id])
   end
+  
   def new 
     @article = Article.new
   end
+  
   def create
     @article = Article.new(title: "...", body: "...")
 
@@ -22,4 +25,18 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :body)
     end
+
+ def edit 
+    @article = Article.find(params[:id])
+ end
+
+ def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+        redirect_to @article
+    else
+        render :edit, status: :unprocessable_entity
+    end
+
 end
